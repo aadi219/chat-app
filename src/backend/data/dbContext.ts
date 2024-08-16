c930ba431c978972fc761ac4845a0cd8abf07ae4
aadi219 <aaadibadola@gmail.com>
2024-08-15 22:43:13 -0400

    setup database connection

---
+++ src/backend/data/dbContext.ts
@@ -0,0 +1,56 @@
+// TODO: Implement migrations
+
+import { Sequelize } from "sequelize";
+import Models from "../models/index.js";
+
+async function connectToDB(config: {
+  dbName: string;
+  username: string;
+  password: string;
+  host: string;
+  port: string;
+}) {
+  const { dbName, username, password, host, port } = { ...config };
+  const db = new Sequelize(dbName, username, password, {
+    host: host,
+    port: parseInt(port),
+    dialect: "postgres",
+  });
+  try {
+    // test db connection
+    await db.authenticate();
+    console.log("Connection to database established");
+
+    // load models and sync changes. NOTE: this does not create migrations
+    loadModels(db);
+    // sync({alter}) - checks current state of tables in DB and performs necessary changes to make it match the model
+    await db.sync({ alter: true });
+    console.log("Models synced successfully");
+
+    // return db instance
+    return db;
+  } catch (err) {
+    console.error("Could not connect to database:", err);
+  }
+}
+
+function loadModels(context: Sequelize) {
+  const { User, Chat, Message } = { ...Models };
+  // define models;
+  const user = User(context);
+  const chat = Chat(context);
+  const msg = Message(context);
+
+  // define relationships
+  // foreignKey option used to explicitly define column name. Must be included in both sides of the relationship.
+  chat.hasMany(msg, { foreignKey: "chatID" });
+  msg.belongsTo(chat, { foreignKey: "chatID" });
+
+  user.hasMany(msg, { foreignKey: "userID" });
+  msg.belongsTo(user, { foreignKey: "chatID" });
+
+  user.belongsToMany(chat, { through: "chat_user", foreignKey: "userID", otherKey: "chatID" });
+  chat.belongsToMany(user, { through: "chat_user", foreignKey: "chatID", otherKey: "userID" });
+}
+
+export default connectToDB;
--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,13 +1,28 @@
 import express, { Express, Request, Response } from "express";
+import "dotenv/config";
+import connectToDB from "./data/dbContext.js";
 
-const app: Express = express();
-const PORT = 3000;
+const main = async () => {
+  const app: Express = express();
+  const PORT = 3000;
 
-app.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`Backend running on PORT:${PORT}`);
-})
+  });
 
-app.get('/', (req: Request, res: Response) => {
+  const dbConfig= {
+    dbName: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, 
+    host: process.env.DB_HOST, 
+    port: process.env.DB_PORT,
+  };
+  const db = await connectToDB(dbConfig);
+
+  app.get("/", (req: Request, res: Response) => {
     console.log('Accessed route "/"');
-    res.send('Response from /');
-})
+    res.send("Response from /");
+  });
+};
+
+main();
